b2db339da923225ac5a8f3a3822f93b48ef2394e
JoshuaHolloway <jhollow6@asu.edu>
2020-12-25 09:25:22 -0600

    added logic for when timer <= 0

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 const min_elem = document.querySelector('#min');
 const sec_elem = document.querySelector('#sec');
 
-let time = 62;
+let time = 2;
 
 const f = () => {
   console.log(time);
 
   const [min, sec] = convert_from_secs(time);
 
-  min_elem.textContent = String(min).padStart(2, '0');
-  sec_elem.textContent = String(sec).padStart(2, '0');
+  if (time === 0) {
+    console.log('TIMER END!');
+  } if (time < 0) {
+    min_elem.textContent = '00';
+    sec_elem.textContent = '00';
+  } else {
+    min_elem.textContent = String(min).padStart(2, '0');
+    sec_elem.textContent = String(sec).padStart(2, '0');
+  }
 
   time--;
 };

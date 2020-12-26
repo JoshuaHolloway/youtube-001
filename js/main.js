438906f7c54465cdd31bb5b97cf4ad4080fc2e04
JoshuaHolloway <jhollow6@asu.edu>
2020-12-25 22:59:16 -0600

    added reset button and wired it up

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
+// TODO: Change this to the value entered from user
+const INITIAL_TIME = 2;
+
+let time = INITIAL_TIME;
+
 const min_elem = document.querySelector('#min');
 const sec_elem = document.querySelector('#sec');
 const circle_elem = document.querySelector('.circle');
 const section3_elem = document.querySelector('.section-3');
+const pills_inner = document.querySelectorAll('.pills-inner');
+
+pills_inner[2].addEventListener('click', () => {
+  console.log('reset pill clicked');
+
+  // Reset time (for countdown logic)
+  time = INITIAL_TIME;
+  const [min, sec] = convert_from_secs(time);
+  
+  // Reset display
+  min_elem.textContent = String(min).padStart(2, '0');
+  sec_elem.textContent = String(sec).padStart(2, '0');
+
+  // Stop flashing:
+  clearInterval(flashing_interval_id);
+  section3_elem.style.backgroundColor = '#161932';
+});
 
 let alarm_colors = [];
 for (let i = 1; i <= 3; ++i) {
@@ -12,7 +34,8 @@ for (let i = 1; i <= 3; ++i) {
 }
 // console.log('alarm_colors: ', alarm_colors);
 
-let interval_id = null;
+let countdown_interval_id = null;
+let flashing_interval_id = null;
 circle_elem.addEventListener('click', () => {
 
   console.dir(circle_elem);
@@ -25,11 +48,11 @@ circle_elem.addEventListener('click', () => {
   console.log('AFTER SetTimeOut()');
  
   // Only run when timer >= 0
-  interval_id = setInterval(f, 1000);
+  countdown_interval_id = setInterval(f, 1000);
 });
 
 
-let time = 2;
+
 
 const f = () => {
   console.log(time);
@@ -38,13 +61,13 @@ const f = () => {
 
   if (time === 1) {
     let count = 0;
-    setInterval(() => {
+    flashing_interval_id = setInterval(() => {
       if(count % 2 === 0) { // even
         section3_elem.style.backgroundColor = alarm_colors[1];
       } else  {// odd
         section3_elem.style.backgroundColor = alarm_colors[2];
       }
-        
+    
       count++;
     }, 1000);
   } if (time <= 0) {
@@ -52,7 +75,7 @@ const f = () => {
     sec_elem.textContent = '00';
 
     // Stop decrementing the timer
-    clearInterval(interval_id);
+    clearInterval(countdown_interval_id);
   } else {
     min_elem.textContent = String(min).padStart(2, '0');
     sec_elem.textContent = String(sec).padStart(2, '0');
